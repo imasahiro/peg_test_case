08495eef8b30293e6a32912a3e8a68908ad0e18f
Masahiro Ide <imasahiro9@gmail.com>
2015-08-09 01:52:13 +0900

    fix run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-TEST_DIR=`dirname $0`
+TEST_DIR=`dirname $0`/../test/
+TEST_DIR=`realpath $TEST_DIR`
 
 for peg in `ls ${TEST_DIR}/pass*.nez`; do
     path=${peg%.*}
     input="$path.txt"
-    echo $path
-    echo $input
+    echo "running " $peg
+    # echo $input
 done
